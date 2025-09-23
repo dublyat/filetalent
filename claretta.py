7124e800b70b0aa7378f041b85cc8f03e176c7bf
dublyat <whit3.ducc@gmail.com>
2025-09-24 05:24:01 +0700

    Update claretta.py

--- claretta.py
+++ claretta.py
@@ -59,14 +59,14 @@ async def send_random_messages():
         for group_id in group_ids:
             message_auto = random.choice(automsg)
             message = await client.send_message(group_id, message_auto)        
-            await asyncio.sleep(random.randint(90,180))
+            await asyncio.sleep(random.randint(120,180))
 
 async def send_random_messages2():
       while True:
         for group_meta in group_metas:
             message_auto = random.choice(automsg)
             message = await client.send_message(group_meta, message_auto)        
-            await asyncio.sleep(random.randint(90,120))
+            await asyncio.sleep(random.randint(180,240))
 
 
 async def main():

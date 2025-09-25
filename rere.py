a6643d6ec54a9eb265b1027f897f5e834051014c
dublyat <whit3.ducc@gmail.com>
2025-09-26 06:14:11 +0700

    Update rere.py

--- rere.py
+++ rere.py
@@ -1,1 +1,92 @@
+from telethon import TelegramClient, events
+import asyncio
+import random
+import getpass
+from random import randint
+from time import sleep
+from telethon import types
+from telethon.errors import SessionPasswordNeededError
 
+###############INPUT PART###############
+
+api_id = '28119255'
+api_hash = '337f330b7ab0c169c6296bc80338c6fd'
+phone = '+6283185102534'
+
+group_metas = [-1001707678506, -1001510261210]  
+
+group_ids = [-1001634555044, -1002422141340]  
+
+
+automsg = ["nenenin dong sayangg @rebeurbae",
+"crtin muka aku dongg @rebeurbae",
+"sini aku tmnin km crt @rebeurbae",
+"vc nakal yuk bebb @rebeurbae",
+"vcsange yuu siniii @rebeurbae",
+"-1 co hyper yg suka kasarin @rebeurbae 😋",
+"-1 co yang tahan 3 ronde @rebeurbae 🥺",
+"-1 co hypersx aht ahtt sayang @rebeurbae 🤤",
+"-1 co yang mau dienakin @rebeurbae 😜",
+"-1 co yang mau vcsex @rebeurbae 🥵",
+"udh becek nih beb @rebeurbae",
+"pgn di clmekin km @rebeurbae",
+"-1 co yang berurat @rebeurbae",
+"suka yang berurat @rebeurbae",
+"pengen crt di mulut @rebeurbae",
+"nenenin dong sayangg 😝 @rebeurbae",
+"crtin muka aku dongg 😍 @rebeurbae",
+"sini aku tmnin km crt 💋 @rebeurbae",
+"vc nakal yuk bebb 🥵 @rebeurbae",
+"vcsange yuu siniii 🤭 @rebeurbae",
+"-1 co hyper yg suka kasarin @rebeurbae",
+"-1 co yang tahan 3 ronde @rebeurbae",
+"-1 co hypersx aht ahtt sayang @rebeurbae",
+"-1 co yang mau dienakin @rebeurbae",
+"-1 co yang mau vcsex @rebeurbae",
+"udh becek nih beb 😋 @rebeurbae",
+"pgn di clmekin km 😭 @rebeurbae",
+"-1 co yang berurat 😫 @rebeurbae",
+"suka yang berurat 🥺 @rebeurbae",
+"pengen crt di mulut 🤪 @rebeurbae"
+]
+
+client = TelegramClient('rere', api_id, api_hash)
+
+###############INPUT PART###############
+
+async def send_random_messages():
+      while True:
+        for group_id in group_ids:
+            message_auto = random.choice(automsg)
+            message = await client.send_message(group_id, message_auto)        
+            await asyncio.sleep(random.randint(120,180))
+
+async def send_random_messages2():
+      while True:
+        for group_meta in group_metas:
+            message_auto = random.choice(automsg)
+            message = await client.send_message(group_meta, message_auto)        
+            await asyncio.sleep(random.randint(180,240))
+
+
+async def main():
+    await client.connect()
+
+    if not await client.is_user_authorized():
+        await client.send_code_request(phone)
+        code = input('Enter the code: ')
+        try:
+            await client.sign_in(phone, code)
+        except SessionPasswordNeededError:
+            password = getpass.getpass('ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴘᴀssᴡᴏʀᴅ: ')
+            await client.sign_in(password=password)
+
+    print("sᴛᴀʀᴛɪɴɢ ᴄʟᴀʀᴇᴛᴛᴀ")
+
+    asyncio.create_task(send_random_messages())
+
+    asyncio.create_task(send_random_messages2())
+      
+    await client.run_until_disconnected()
+
+asyncio.run(main())

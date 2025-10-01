b10f6261d0ef2909179495f1623c86970cb5d65a
dublyat <whit3.ducc@gmail.com>
2025-10-02 04:17:56 +0700

    Update claretta.py

--- claretta.py
+++ claretta.py
@@ -13,9 +13,11 @@ api_id = '28119255'
 api_hash = '337f330b7ab0c169c6296bc80338c6fd'
 phone = '+6283185102534'
 
-group_metas = [-1001707678506, -1001510261210]  
+group_metas = [-1001707678506]  
 
-group_ids = [-1001634555044, -1002422141340]  
+group_ids = [-1001510261210, -1001634555044, -1002422141340, -1001527830462] 
+
+TARGET_GROUPS = [-1002288646386, -1001202193997]
 
 
 automsg = ["wanna kolsegs byy? @babyeta",
@@ -52,22 +54,57 @@ automsg = ["wanna kolsegs byy? @babyeta",
 
 client = TelegramClient('claretta', api_id, api_hash)
 
-###############INPUT PART###############
+is_forwarding_active = False
+
+############### BOT SPAM ###############
 
 async def send_random_messages():
       while True:
         for group_id in group_ids:
             message_auto = random.choice(automsg)
             message = await client.send_message(group_id, message_auto)        
-            await asyncio.sleep(random.randint(120,180))
+            await asyncio.sleep(random.randint(300,900))
 
 async def send_random_messages2():
       while True:
         for group_meta in group_metas:
             message_auto = random.choice(automsg)
             message = await client.send_message(group_meta, message_auto)        
-            await asyncio.sleep(random.randint(180,240))
+            await asyncio.sleep(random.randint(600,1200))
 
+############### BOT FORWARD ###############
+
+async def forward_loop():
+    global is_forwarding_active
+    group_index = 0
+
+    while is_forwarding_active:
+        saved = await client.get_messages('me', limit=20)
+        if saved:
+            msg = random.choice(saved)
+            target = TARGET_GROUPS[group_index]
+
+            await client.forward_messages(
+                entity=target,
+                messages=msg 
+            )
+            print(f"Forwarded message ID {msg.id} to {target}")
+
+            group_index = 1 - group_index
+            await asyncio.sleep(random.randint(600,1200))
+
+
+@client.on(events.NewMessage(pattern='/blackcat'))
+async def cmd_start(event):
+    global is_forwarding_active
+    if not is_forwarding_active:
+        is_forwarding_active = True
+        await event.respond("ᴇɴɢɪɴᴇ sᴛᴀʀᴛᴇᴅ")
+        asyncio.get_event_loop().create_task(forward_loop())
+    else:
+        await event.respond("ᴇɴɢɪɴᴇ ᴀʟʀᴇᴀᴅʏ ʀᴜɴɴɪɴɢ")
+
+############### LOGIN PART ###############
 
 async def main():
     await client.connect()
@@ -81,7 +118,7 @@ async def main():
             password = getpass.getpass('ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴘᴀssᴡᴏʀᴅ: ')
             await client.sign_in(password=password)
 
-    print("sᴛᴀʀᴛɪɴɢ ᴄʟᴀʀᴇᴛᴛᴀ")
+    print("sᴛᴀʀᴛɪɴɢ ʀᴇʀᴇ")
 
     asyncio.create_task(send_random_messages())
 
